13901b1502919b8636d1ea772a0b47b18c476bc7
Ruddy Mejia Mamani <rmejiam@est.emi.edu.bo>
2020-10-21 01:58:31 -0400

    pruebas2

--- prueba.java
+++ prueba.java
@@ -2,5 +2,6 @@ public class prueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Control1");
+        System.out.println("Control2");
     }
 }
